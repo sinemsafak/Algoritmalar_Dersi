a884703ea9df49b0735d386dca0c2b0eb4b50cfc
sinemsafak <118027044+sinemsafak@users.noreply.github.com>
2022-11-27 18:09:09 +0300

    Add files via upload

---
+++ algoritmalar/hafta5/src/sonsatir.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class sonsatir {
+    public static void main(String[] args) {
+        String path = "src/output.txt";
+        try {
+            File f = new File(path);
+            Scanner scan = new Scanner(f);
+            String[] topla = new String[8];
+            for (int i = 0; i < topla.length; i++) {
+                topla[i] = scan.nextLine();
+                if (topla[6] != null) {
+                    System.out.println(topla[6]);
+                }
+
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+}
